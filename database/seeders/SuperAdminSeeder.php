4e83096ab5cae3a1caf61728390d55203701c851
alief1206 <fqturalief15@gmail.com>
2026-09-22 09:36:24 +0700

    data dumy

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -12,6 +12,19 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
+        // 1. admin@silila.com (Super Admin)
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin@silila.com'],
+            [
+                'nama' => 'Super Admin',
+                'password' => bcrypt('password123'),
+                'role' => '1',
+                'nip' => '123456789012345678',
+                'telp' => '081234567890',
+            ]
+        );
+
+        // 2. superadmin@gmail.com (Super Administrator)
         \App\Models\User::updateOrCreate(
             ['email' => 'superadmin@gmail.com'],
             [
@@ -20,6 +33,18 @@ class SuperAdminSeeder extends Seeder
                 'role' => '1',
                 'nip' => '0000000000',
                 'telp' => '0000000000',
+            ]
+        );
+
+        // 3. admin_chat@silila.com (Admin Chat)
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin_chat@silila.com'],
+            [
+                'nama' => 'Admin Chat',
+                'password' => bcrypt('password123'),
+                'role' => '3',
+                'nip' => 'admin',
+                'telp' => '08123456789',
             ]
         );
     }
